f3a56d3d7a2d0614413cf675ce9d6a2c10b13edc
AaronOwensSE <aaron.owens.se@gmail.com>
2025-10-04 21:23:29 -0500

    testing: Configure project for Jest testing.
    And update lessons learned.

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+export default {
+    testEnvironment: "node",    // Use a Node.js environment for testing, as opposed to simulating a browser environment.
+    transform: {},              // Prevent Jest from using Babel to transform code, allowing it to run ES Modules.
+};
