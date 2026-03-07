268d1cd66a4beeab5be00979025dae9de26b74b5
AaronOwensSE <aaron.owens.se@gmail.com>
2026-03-07 15:53:29 -0600

    Add DB schema creation and deletion scripts. Move constants up to project level.

---
+++ constants.js
@@ -0,0 +1,31 @@
+// =================================================================================================
+// Public API
+// =================================================================================================
+const USER_ID_MIN_LENGTH = 3;
+const USER_ID_MAX_LENGTH = 30;
+const USER_PASSWORD_MIN_LENGTH = 12;
+const USER_PASSWORD_MAX_LENGTH = 30;
+const USER_NAME_MIN_LENGTH = 3;
+const USER_NAME_MAX_LENGTH = 60;
+const USER_EMAIL_MIN_LENGTH = 7;
+const USER_EMAIL_MAX_LENGTH = 254;
+const SALT_DEFAULT_ROUNDS = 10;
+
+const GROUP_NAME_MIN_LENGTH = 3;
+const GROUP_NAME_MAX_LENGTH = 30;
+
+const constants = {
+    USER_ID_MIN_LENGTH,
+    USER_ID_MAX_LENGTH,
+    USER_PASSWORD_MIN_LENGTH,
+    USER_PASSWORD_MAX_LENGTH,
+    USER_NAME_MIN_LENGTH,
+    USER_NAME_MAX_LENGTH,
+    USER_EMAIL_MIN_LENGTH,
+    USER_EMAIL_MAX_LENGTH,
+    SALT_DEFAULT_ROUNDS,
+    GROUP_NAME_MIN_LENGTH,
+    GROUP_NAME_MAX_LENGTH
+};
+
+export default constants;
